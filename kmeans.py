2298074fe6140adc37ff5c9f2aa10693be697bc7
Ilana Zane <46580323+ilanazane@users.noreply.github.com>
2020-04-27 19:06:43 -0400

    tried to create overall training_model function..

--- kmeans.py
+++ kmeans.py
@@ -127,7 +127,6 @@ def toGrey(image):
     for i in range(0,len(image)):
         for j in range(0, len(image[i])):
             image[i][j] = 0.21*image[i][j][0] + 0.72*image[i][j][1] + 0.07*image[i][j][2]
-
     return image
 
 
--- nn.py
+++ nn.py
@@ -8,29 +8,27 @@ def random_weights():
 	return weight1, weight2
 
 def sigmoid(arr):
-	result = []
-	for x in arr:
-		result.append(1/(1 + np.exp(-x))) 
+	result = 1/(1 + np.exp(-arr))
 	return result
 
 def sigmoid_prime(arr):
-	result = []
-	for x in arr:
-		result.append(np.exp(-x)/np.square(1 + np.exp(-x)))
+	result = np.exp(-arr)/np.square(1 + np.exp(-arr))
 	return result
 
+#input_layer is 1D array
+def calc_layers(weight1, weight2, input_layer):
+	hidden_layer = sigmoid(np.dot(weight1,input_layer))
+	#hidden_layer = sigmoid(hidden_layer)
+	output_layer = sigmoid(np.dot(weight2,hidden_layer))
+	#output_layer = sigmoid(output_layer)
+	return hidden_layer,output_layer
 
-def train_model(weight1, weight2, input_layer):
-	hidden_layer = np.dot(weight1,input_layer)
-	hidden_layer = sigmoid(hidden_layer)
-	output_layer = np.dot(weight2,hidden_layer)
-	output_layer = sigmoid(output_layer)
 
 
 #calculate the cost
 def calc_cost(output_layer, y_layer):
 	sum = 0
-	for a in range(length(output_layer)):
+	for a in range(len(output_layer)):
 		sum += np.square(output_layer[a] - y_layer[a])
 	return sum
 
@@ -62,11 +60,34 @@ def calc_cost_deriv_2(input_layer, hidden_layer, y_layer, weight2, weight1):
 
 
 
+def training_data():
+	img = cv2.imread('painting.jpg')
+	#img = cv2.imread('test2.jpg')
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+	half = int(len(img[0])/2)
+	left = img[:,:half]
+
+	toGrey(left)
+	
+	patches=[]
+	#iterate through grayleft
+	#iterate through rows
+	for i in range(1,len(grayleft)-1):
+    	#iterate through columns
+    	for j in range(1,len(grayleft[0])-1):
+        	#grayleft[i][j] starts on middle pixel
+        	#find the rest of the patch (adjacent pixels)
+        	patches.append((grayleft[i-1:i+2,j-1:j+2],(i,j)))
+
+
+def train_model(input_data,)
+
+
+
 
 
 
 #grayleftPatch = return_grey()
 weight1, weight2 = random_weights()
-input_layer = grayleftPatch[1][1]
+input_data = grayleftPatch[1][1]
 y_layer = img[1][1] #feed in image as some sort of input somehow somewhere someday not today
-
